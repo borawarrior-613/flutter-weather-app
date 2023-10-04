ca73b151ab0874a03085b1c239522739259e687e
borawarrior-613 <sriya.makam@gmail.com>
2023-10-05 00:33:08 +0530

    Upload Files

---
+++ lib/controllers/weather_controller.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:week_4/models/weather_model.dart';
+
+class WeatherController {
+  final WeatherModel weatherModel = WeatherModel();
+
+  Future<void> fetchWeatherData(String city) async {
+    const apikey = '9c02ff3d1b523344991364aef39b5551';
+    final apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apikey&units=metric';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final result = jsonDecode(response.body);
+      weatherModel.cityName = result['name'];
+      weatherModel.temperature = result['main']['temp'].toString();
+      weatherModel.iconUrl =
+          'http://openweathermap.org/img/w/${result['weather'][0]['icon']}.png';
+      weatherModel.status = 1;
+    } else if (response.statusCode == 404) {
+      weatherModel.status = -1;
+      throw FlutterError.fromParts(
+        <DiagnosticsNode>[
+          ErrorSummary(
+              'This place does not exist! \nResponse code: ${response.statusCode}'),
+        ],
+      );
+    }
+  }
+}
